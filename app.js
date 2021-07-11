0d8bc78a1155bd0d7f05c66b12dc842d4467a41c
Balog-dev <ridwanbalogun511@gail.com>
2021-07-12 03:43:37 +1400

    skills

--- app.js
+++ app.js
@@ -82,8 +82,32 @@ clearAllE1.addEventListener('click', (e)=> {
     dis2Num = '';
     result = '';
     tempResultE1.innerText = '0';
-};
+});
+clearLastE1.innerText('click', (e)=> {
+    display2E1.innerText = '';
+    dis2Num = ''; 
+
+});
+
+window.addEventListener('keydown', (e)=> {
+    if(
+        e.key === '0' ||
+        e.key === '1' ||
+        e.key === '2' ||
+        e.key === '3' ||
+        e.key === '4' ||
+        e.key === '5' ||
+        e.key === '6' ||
+        e.key === '7' ||
+        e.key === '8' ||
+        e.key === '9' ||
+    ){
+        clickButtonE1(e.key);
+    }
+});
 
 
-)
+
+
+
 
